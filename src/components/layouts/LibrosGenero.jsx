f19c67059236dfc764d31390364ecc00beda49ed
JohnHenry <john_henry.4motmail.com>
2023-10-19 10:14:10 -0500

    se ajustan los contenedore de las secciones libro, genero y las secciones internas

--- src/components/layouts/LibrosGenero.jsx
+++ src/components/layouts/LibrosGenero.jsx
@@ -6,32 +6,37 @@ function LibrosGenero() {
   return (
     <section className='libros'>
         <Header />
-    <section className='generos'>
-      <section className='librosCoverUno'>
-        <img src={portada} height={100} width={100} />
+      <section className='generos'>
+        <section className='seccionSuperior'>
+        <section className='librosCoverUno'>
+          <img src={portada} height={100} width={100} />
+          <h1>ABCDE</h1>
+        </section>
+        <section className='librosCoverDos'>
+          <img src={portada} height={100} width={100} />
+        </section>
+        <section className='librosCoverTres'>
+          <img src={portada} height={100} width={100} />
+        </section>
+        <section className='librosCoverCuatro'>
+          <img src={portada} height={100} width={100} />
+        </section>
+        </section>
+        <section className='seccionInferior'>
+        <section className='librosCoverQuinto'>
+          <img src={portada} height={100} width={100} />
+        </section>
+        <section className='librosCoverSexto'>
+          <img src={portada} height={100} width={100} />
+        </section>
+        <section className='librosCoverSeptimo'>
+          <img src={portada} height={100} width={100} />
+        </section>
+        <section className='librosCoverOctavo'>
+          <img src={portada} height={100} width={100} />
+          </section>
+        </section>
       </section>
-      <section className='librosCoverDos'>
-        <img src={portada} height={100} width={100} />
-      </section>
-      <section className='librosCoverTres'>
-        <img src={portada} height={100} width={100} />
-      </section>
-      <section className='librosCoverCuatro'>
-        <img src={portada} height={100} width={100} />
-      </section>
-      <section className='librosCoverQuinto'>
-        <img src={portada} height={100} width={100} />
-      </section>
-      <section className='librosCoverSexto'>
-        <img src={portada} height={100} width={100} />
-      </section>
-      <section className='librosCoverSeptimo'>
-        <img src={portada} height={100} width={100} />
-      </section>
-      <section className='librosCoverOctavo'>
-        <img src={portada} height={100} width={100} />
-      </section>
-    </section>
     </section>
   )
 }
